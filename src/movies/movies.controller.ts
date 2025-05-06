540828990c37c317f3b46e33fb533951c85530ad
jeongsangyoung <jeongsangyoung@naver.com>
2025-05-07 00:51:53 +0900

    made REST API

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Put('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.movieService.update(movieId, updateData);
+  }
+}
